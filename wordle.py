7b9fc4cb6b108bb1464057b56925e7e35cab5bbe
Alex Beals <alexcbeals@gmail.com>
2022-01-03 14:34:02 -0800

    Build out scoring function for method with timing, and evaluate my simple version

--- wordle.py
+++ wordle.py
@@ -1,6 +1,6 @@
 
-import random, pickle
-from typing import List, Tuple
+import random, pickle, time
+from typing import List, Tuple, Callable
 
 ScoredWord = List[Tuple[str, int]]
 
@@ -39,19 +39,40 @@ with open('valid_answers', 'rb') as fp:
 # Keep guessing until you get the word
 #
 # We assume that we don't know which are valid answers for now
+# Played 10000 games in 99.4 seconds and found the word in an average of 
+# 5.8204 guesses, though the worst case took 15 guesses.
+def play_dumb() -> int:
+    goal_word = random.choice(valid_answers)
 
-goal_word = random.choice(valid_answers)
-print("The goal word is: '{0}'".format(goal_word))
+    guesses = 0
+    possible_words = valid_words
+    while (True):
+        guesses += 1
+        guess = random.choice(possible_words)
+        scored_word = score_word(goal_word, guess)
+        possible_words = [word for word in possible_words if matches_scored_word(word, scored_word) and word != guess]
+        if (guess == goal_word):
+            break
+    return guesses
 
-guesses = 0
-possible_words = valid_words
-while (True):
-    guesses += 1
-    guess = random.choice(possible_words)
-    scored_word = score_word(goal_word, guess)
-    possible_words = [word for word in possible_words if matches_scored_word(word, scored_word) and word != guess]
-    if (guess == goal_word):
-        print("Correctly guessed '{0}' with {1} total guesses!".format(guess, guesses))
-        break
-    else:
-        print("Guess #{0} is '{1}', reducing down to {2} still valid words.".format(guesses, guess, len(possible_words)))
+def evaluate_method(total_games_to_play: int, method: Callable[[], int]):
+    total_guesses = 0
+    max = 0
+    start = time.time()
+    for _ in range(0, total_games_to_play):
+        guesses = method()
+        if guesses > max:
+            max = guesses
+        total_guesses += guesses
+
+    print(
+        ("Played {0} games in {1} seconds and found the word in an average of "+
+        "{2} guesses, though the worst case took {3} guesses.").format(
+            total_games_to_play,
+            round(time.time() - start, 1),
+            round(float(total_guesses) / total_games_to_play, 2),
+            max
+        )
+    )
+
+evaluate_method(10000, play_dumb)
